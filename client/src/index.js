e6e49462706e65dd043ace063a8319eaa167854e
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-22 01:52:42 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 
 
 import App from './App';
-import './styles.css'; 
+import './styles.css'; //global styles
 
-//to get our context inside of components
+//To see how can we hook this up to our components
+//to get our context inside of components, import ContextProvider in index.js file/src
 import { ContextProvider } from './SocketContext.js';
 
 
 ReactDOM.render(
+    //wrap app with ContextProvider
     <ContextProvider>
         <App />
     </ContextProvider>,
  document.getElementById('root') 
+ //having document that we set the application to our div root
+ //entire react application hook onto this root div
 );
  
 
