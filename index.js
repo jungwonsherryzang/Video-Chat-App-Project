0aa483cee71e0633487dc92295a8bb46d2db303e
sherry jungwon zang <91277856+jungwonsherryzang@users.noreply.github.com>
2022-06-23 07:09:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,32 @@
 //BACKEND SIDE
-//import app from 'express';
-//import cors from 'cors'; 
+
 const app = require("express")();
-const server = require("http").createServer(app); //http: built-in node module
-const cors = require("cors"); //for cross-origin request->useful when we deploy application
+const server = require("http").createServer(app);
+const cors = require("cors"); 
 
-//initialize empty reactapp
 
-//setting io server side instance
 const io = require("socket.io")(server, {
     cors: {
-        origin: "*", //allow access from all origins
+        origin: "*", 
         methods: ["GET", "POST"]
     }
 });
 
-//express.js
-//to mount the specified middleware functions at the path->(path, callback)
+
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-//creating first route
-//"/":root route
-//router: keeps your UI in sync with the URL
-app.get("/", (req, res) => {
-    res.send('Server is running'); //when user visits server, will get this message
-})
-//----------------------------------server setup-------------------------------------
 
-//----------------------------------socket setup-------------------------------------
-//socket is using for real-time data transmission
+app.get("/", (req, res) => {
+    res.send('Server is running'); 
+})
+
+
+
 io.on('connection', (socket) => {
-    socket.emit('me', socket.id); //emit the id as soon as the socket opens
-    //will give certain id on the frontend side
-    console.log(socket.id); //TRY
+    socket.emit('me', socket.id); 
+    console.log(socket.id);
     
     
     //socket handlers
@@ -43,7 +35,6 @@ io.on('connection', (socket) => {
     });
     
     socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-        //.to().emit: emitting to 'whom'
         io.to(userToCall).emit(
             "calluser", { signal: signalData, from, name });
         })
